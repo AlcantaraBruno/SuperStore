37a0bad0f0cfdde4910a8dfa1234acfe2a1e73dc
Bruno Alcantara <brunnoalcantara97@gmail.com>
2020-07-03 23:21:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SuperStore
+ 
---
+++ core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#class dos produtos
+class Product (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    #lembrar modificar o tipo do price
+    price = models.CharField(max_length = 11)
+    photo = models.ImageField()
+    #desafio
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    #criar um id automaticamente
+    def __str__ (self):
+        return str(self.id)
+
+    #renomear a tabela como vai ser no banco
+    class Meta:
+        db_table = 'Produto'
